a86db2e9a12e4c3ee3b3c9518e119914a973932f
Jaison Mathew <mjaison29@gmail.com>
2020-05-20 11:46:32 -0400

    created javascript function
    Creates function that counts the number of clicks of the reset button in
    contact webpage.

--- JMscript.js
+++ JMscript.js
@@ -1,8 +1,15 @@
 console.dir(document);
 console.log('me');
 var a = "\"Hanging\"";
-var b = 10;
+var b = 0;
 var c;
 c = a + ' ' + b;
 console.log(c);
 console.log(typeof(b));
+
+function counter(x){
+    b++;
+    var posts = x + b 
+    document.querySelector('section').innerHTML = posts;
+    console.log(x);
+}
