97e29c2f2f270771fb67616c7a6aec00e2f79554
Jaison Mathew <mjaison29@gmail.com>
2020-05-20 14:00:13 -0400

    Added object and array
    created object and array in javascript file.

--- JMscript.js
+++ JMscript.js
@@ -13,3 +13,21 @@ function counter(x){
     document.querySelector('section').innerHTML = posts;
     console.log(x);
 }
+
+function multiply(p, q){
+    return p*q;
+}
+
+var car = {};
+car.year = 1984;
+car.type = 'pontiac';
+car.name = 'firebird';
+car.message = function(){
+    return 'Its an old car.';
+}
+
+var favMovies = ["Back to the future", 1985, "Avengers Endgame", 2019]
+favMovies[favMovies.length] = "Lord of the Rings"
+favMovies[favMovies.length] = 2003
+
+//document.querySelector('h2').innerHTML = car.name + '' + car.year;
