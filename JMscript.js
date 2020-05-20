9b9e679c9f9a90ca18f0af0c2a9336bcd528e015
Jaison Mathew <mjaison29@gmail.com>
2020-05-20 14:26:31 -0400

    Testing if statement
    Created if statement in javascript file for testing.

--- JMscript.js
+++ JMscript.js
@@ -29,5 +29,20 @@ car.message = function(){
 var favMovies = ["Back to the future", 1985, "Avengers Endgame", 2019]
 favMovies[favMovies.length] = "Lord of the Rings"
 favMovies[favMovies.length] = 2003
+var soda = favMovies.pop();
+console.log(favMovies);
+var move = favMovies.unshift('2012');
+console.log(favMovies);
+var clean = favMovies.reverse();
+console.log(favMovies);
+
+var num = 12;
+if(num == 12 && b == 0){
+    console.log('both are equal');
+}else if(num < 11){
+    console.log('num is less than 11');
+}else{
+    console.log('num is greater than 12');
+}
 
 //document.querySelector('h2').innerHTML = car.name + '' + car.year;
