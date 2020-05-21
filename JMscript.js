8cacf3f8b00231026d73e3b36a9c0e5edf446200
Jaison Mathew <mjaison29@gmail.com>
2020-05-21 10:18:50 -0400

    Added test file for practice
    Created html file to test javascript coding

--- JMscript.js
+++ JMscript.js
@@ -30,6 +30,15 @@ car.message = function(){
     return 'Its an old car.';
 }
 
+var game = {};
+game.name = "Mario Kart 64";
+game.year = 1996;
+game.platform = "Nintendo 64";
+game.introMessage = function(){
+    return 'Welcome to Mario Kart.';
+}
+
+
 var favMovies = ["Back to the future", 1985, "Avengers Endgame", 2019]
 favMovies[favMovies.length] = "Lord of the Rings"
 favMovies[favMovies.length] = 2003
@@ -55,6 +64,50 @@ for(var i =0; i<= favMovies.length; i++){
 
 for(var t in car){
     console.log(t + ': ' + car[t])
+}
+
+for(var play in game){
+    console.log(play + ': ' + game[play])
+}
+
+var myString = "I look forward to working with you."
+var find = myString.indexOf('to');
+console.log(find);
+var look = myString.search('you')
+console.log(look);
+var letterIndex = myString.lastIndexOf('forward');
+console.log(letterIndex);
+
+/*const myElement = document.querySelector('h2')
+function buttonPress(){
+    let temp = document.getElementById("myInput");
+    output(temp.value);
+}
+
+function output(mess){
+    myElement.innerHTML = mess;
+}
+
+const myButton = document.querySelector('#buttonTest');
+myButton.addEventListener('click', buttonPress);*/
+
+const output = document.getElementById('output');
+const testingButton = document.getElementById('testingButton');
+testingButton.addEventListener('click', checkVal);
+function checkVal(){
+    let val = document.querySelector('input[name="myNum"]').value;
+    console.log(val);
+    output.innerHTML = numChecker(val);
+}
+
+function numChecker(num){
+    console.log(num);
+    if(num%2){
+        message = "its odd";
+    }else{
+        message = "its even";
+    }
+    return message;
 }
 
 //document.querySelector('h2').innerHTML = car.name + '' + car.year;
